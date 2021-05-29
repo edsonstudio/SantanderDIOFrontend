f5415bac4c0c22b78660094adf2c8bb411fb5b26
edsont8 <edsont8@hotmail.com>
2021-05-29 19:46:19 -0300

    Fix logo directory

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { StockCardComponent } from './stock-card/stock-card.component';
 
 
 
 @NgModule({
   declarations: [
     DashboardPageComponent,
+    StockCardComponent,
   ],
   imports: [
     CommonModule
