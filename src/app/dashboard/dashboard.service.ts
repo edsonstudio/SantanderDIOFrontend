642e3fcbb8cfa111161da87ffe0f35881c2b7742
edsont8 <edsont8@hotmail.com>
2021-05-29 22:05:18 -0300

    https

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -7,7 +7,7 @@ import Stock from '../shared/models/stock-model';
 })
 export class DashboardService {
 
-  readonly baseUrl = 'http://bootcamp-java-santander.herokuapp.com/bootcamp';
+  readonly baseUrl = 'https://bootcamp-java-santander.herokuapp.com/bootcamp';
 
   constructor(private http: HttpClient) { }
 
